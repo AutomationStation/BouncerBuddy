de47f1fb5e824ce16ba9d40ab318679b57c8fd1d
CttYss <cathyljxi@gmail.com>
2016-02-22 14:57:38 -0500

    Added files via upload

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  project_feature_login
+//
+//  Created by Huge Face T-T on 2/7/16.
+//  Copyright © 2016 494. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
